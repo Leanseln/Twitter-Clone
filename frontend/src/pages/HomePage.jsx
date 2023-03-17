85575a83b3bad4f483c01e37bb89859cd3b77a89
LeanselN <oleanselnico@gmail.com>
2023-03-17 18:57:14 +0800

    Updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,19 @@
 import '../assets/HomePage.css'
 import { useState } from 'react'
+import {FaEyeSlash, FaEye} from 'react-icons/fa'
 
 const HomePage = () => {
     const [username,setUsername] = useState('')
     const [password,setPassword] = useState('')
+    const [showPassword,toggleShowPassword] = useState(false) 
     const handleClick = (e) => {
         e.preventDefault()
         console.log(username,password)
     }
+    const handleShow = () => {
+        toggleShowPassword(!showPassword)
+    }
+    
 
   return (
     <div className='homepage'>
@@ -20,20 +26,25 @@ const HomePage = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 />
-
-            <input 
-                type="password" 
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
-
-            <button 
+            <div className="password">
+                <input 
+                    type={showPassword ? 'text': 'password'}
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    /> {showPassword ? <FaEye className='silip' onClick={handleShow}/>: <FaEyeSlash className='silip' onClick={handleShow}/>}
+            </div>
+            <button
                 className='btnlog'
                 onClick={handleClick}
                 >Log in</button>
 
             <div className="line"></div>
+
+            <a href="#">Forgot Password?</a>
+        </div>
+        <div className="bottombox">
+            <p>Don't have an account? <a href="">Sign up</a></p>
         </div>
     </div>
   )
